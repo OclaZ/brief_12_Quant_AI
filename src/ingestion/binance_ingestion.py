13e58669d9052896bedb710ec4a49bf7c36b3a79
KarimaChami <karimachami24@gmail.com>
2026-01-20 10:23:10 +0100

    duplicated parts of code

--- src/ingestion/binance_ingestion.py
+++ src/ingestion/binance_ingestion.py
@@ -26,24 +26,18 @@ def data_collection_api():
         "close_time", "quote_asset_volume", "number_of_trades",
         "taker_buy_base_volume", "taker_buy_quote_volume", "ignore"
     ]
-    
-    # Créer un DataFrame pandas
     df = pd.DataFrame(data, columns=columns)
     
-    numeric_cols = ["open", "high", "low", "close", "volume",
-                    "quote_asset_volume", "taker_buy_base_volume", "taker_buy_quote_volume"]
-    df[numeric_cols] = df[numeric_cols].astype(float)
+    # numeric_cols = ["open", "high", "low", "close", "volume",
+    #                 "quote_asset_volume", "taker_buy_base_volume", "taker_buy_quote_volume"]
+    # df[numeric_cols] = df[numeric_cols].astype(float)
     
     # Conversion en datetime **millisecondes** pour compatibilité PySpark
-    df["open_time"] = pd.to_datetime(df["open_time"], unit="ms").astype('datetime64[ms]')
-    df["close_time"] = pd.to_datetime(df["close_time"], unit="ms").astype('datetime64[ms]')
+    # df["open_time"] = pd.to_datetime(df["open_time"], unit="ms").astype('datetime64[ms]')
+    # df["close_time"] = pd.to_datetime(df["close_time"], unit="ms").astype('datetime64[ms]')
     
     return df
 
 df = data_collection_api()
-
 df.to_parquet("data/bronze/btc_minute_data.parquet", engine="pyarrow", index=False)
-
 print("Fichier Parquet sauvegarde")
-
-# bronze -> raw !!!
